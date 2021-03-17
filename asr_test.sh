0c9e5a12be7f2fb5f8c59abb745a439c662035fc
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2021-03-17 00:56:18 +0000

    lm rescoring support

--- asr_test.sh
+++ asr_test.sh
@@ -5,8 +5,8 @@ MDL="QuartzNet15x5Base-En"
 # MDL="QuartzNet15x5NR-En"
 # MDL="Jasper10x5Dr-En"
 
-# TEST_DATA=${DATA}/en/librispeech/dev-clean-2.json
+TEST_DATA=${DATA}/en/librispeech/dev-clean-2.json
 # TEST_DATA=${DATA}/en/webex/webex.tst.json
-TEST_DATA=${DATA}/en/webex/webex.tiny.json
+# TEST_DATA=${DATA}/en/webex/webex.tiny.json
 
-python speech_to_text_infer.py --asr_model=${MDL} --dataset=${TEST_DATA}
+python speech_to_text_infer.py --asr_model=${MDL} --dataset=${TEST_DATA} >> wer.log
