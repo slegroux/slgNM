07a57b6b45857c55c7ccf90db98d50ccfc1bc5be
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2021-05-27 03:46:52 +0000

    grpc streaming

--- grpc/asr_client.py
+++ grpc/asr_client.py
@@ -1,35 +1,83 @@
 #!/usr/bin/env python
+
 import logging
 import grpc
+from numpy.lib.stride_tricks import as_strided
 import asr_service_pb2
 import asr_service_pb2_grpc
 import argparse
-IP='localhost'
-PORT='50051'
-SR=16000
+import numpy as np
+
+IP = 'localhost'
+PORT = '50051'
+SR = 16000
+# CHUNK_SIZE = 1024
+FRAME_LEN = 2
+CHUNK_SIZE = int(FRAME_LEN*SR)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, required=True)
     parser.add_argument('--port', type=int, default=50051)
     parser.add_argument('--sr', type=int, default=16000)
+    parser.add_argument('--streaming', type=bool, default=False)
     return(parser.parse_args())
+
+def gen(audio_file_name):
+	config = asr_service_pb2.RecognitionConfig(
+		sample_rate=16000,
+		audio_encoding = 'LINEAR16_PCM'
+		)
+
+	with open(audio_file_name, 'rb') as f:
+		data = f.read(CHUNK_SIZE)
+		while data != b'':
+			yield asr_service_pb2.RecognizeRequest(
+                config=config,
+                audio=asr_service_pb2.RecognitionAudio(content=data)
+                )
+			data = f.read(CHUNK_SIZE)
+
 
 class AsrServiceClient():
     def __init__(self, ip, port):
         ip_port = f'{ip}:{port}'
         channel = grpc.insecure_channel(ip_port)
         self.stub = asr_service_pb2_grpc.AsrServiceStub(channel)
+        self.config = self.audio_config() 
 
-    def get_transcript(self, audio_path, sr=16000):
-        request_config = asr_service_pb2.RecognitionConfig(sample_rate=sr)
-        request_config.audio_encoding = asr_service_pb2.RecognitionConfig.LINEAR16_PCM
+    @staticmethod
+    def audio_config(sr=SR, encoding='LINEAR16_PCM'):
+        config = asr_service_pb2.RecognitionConfig(
+            sample_rate = sr,
+            audio_encoding = encoding
+        )
+        return config
+
+    def get_from_file(self, audio_path):
         request_audio = asr_service_pb2.RecognitionAudio(uri=audio_path)
-        request = asr_service_pb2.RecognizeRequest(config=request_config, audio=request_audio)
-        return self.stub.Recognize(request).transcript
-    
+        request = asr_service_pb2.RecognizeRequest(config=self.config, audio=request_audio)
+        return self.stub.Recognize(request)
+
+    def get_from_file_stream(self, audio_path):
+        # rand_bytes = np.random.bytes(8)
+        # request_audio = asr_service_pb2.RecognitionAudio(content=rand_bytes)
+        # request = asr_service_pb2.RecognizeRequest(config=self.config, audio=request_audio)
+        return self.stub.StreamingRecognize(gen(audio_path))
+
+    def get_from_mic_stream(self, sr=16000):
+        pass
+
+
 if __name__ == '__main__':
     logging.basicConfig()
     args = get_args()
     client = AsrServiceClient(IP,args.port)
-    print(client.get_transcript(args.path, sr=args.sr))
+    if args.streaming:
+        results = client.get_from_file_stream(args.path)
+        for res in results:
+            print(res)
+    else:
+        print(client.get_from_file(args.path))
+    
--- grpc/asr_server.py
+++ grpc/asr_server.py
@@ -5,17 +5,18 @@ import grpc
 import asr_service_pb2
 import asr_service_pb2_grpc
 from google.protobuf import duration_pb2
-from model_api import initialize_model, transcribe_all
+from model_api import initialize_model, transcribe_all, init_streaming_mdl
 from nemo.utils import logging
-
+import numpy as np
 
 MDL = 'QuartzNet15x5Base-En'
 WAV = '1919-142785-0028.wav'
-N_WORKERS = 50
+N_WORKERS = 10
 
 class AsrServiceServicer(asr_service_pb2_grpc.AsrServiceServicer):
     def __init__(self):
         self.model = initialize_model(MDL)
+        self.streaming_mdl = init_streaming_mdl(MDL)
     
     def Recognize(self, request, context):
         # RecognizeRequest as input RecognizeResponse as output
@@ -25,14 +26,13 @@ class AsrServiceServicer(asr_service_pb2_grpc.AsrServiceServicer):
         return response
     
     def StreamingRecognize(self, request_iterator, context):
-        request = next(request_iterator)
+        # request = next(request_iterator)
         for request in request_iterator:
-            res = "this is"
-            if res:
-                response = asr_service_pb2.RecognizeResponse()
-                response.
-
-
+            data = request.audio.content
+            response = asr_service_pb2.RecognizeResponse()
+            signal = np.frombuffer(data, dtype=np.int16)
+            response.transcript = self.streaming_mdl.transcribe(signal)
+            yield response
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=N_WORKERS))
